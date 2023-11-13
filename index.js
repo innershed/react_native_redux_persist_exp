3f203604e115c755eb2f43a77dc5887d65f6eb56
Lukasz Sibielak <luke.sibielak@gmail.com>
2023-11-13 17:08:41 +0100

    Fixed import.

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
-import {FileSystem} from 'expo';
+import { FileSystem } from "expo-file-system";
 
 let options = {
   path: `${FileSystem.documentDirectory}`,
   encoding: FileSystem.EncodingTypes.UTF8,
-  toFileName: (name) => name.split(':').join('-'),
-  fromFileName: (name) => name.split('-').join(':')
+  toFileName: (name) => name.split(":").join("-"),
+  fromFileName: (name) => name.split("-").join(":"),
 };
 
 const pathForKey = (key) => `${options.path}/${options.toFileName(key)}`;
 
 const ExpoFileSystemStorage = {
-  config: (custom) => { options = {...options, custom}; },
+  config: (custom) => {
+    options = { ...options, custom };
+  },
 
   setItem: async (key, value, callback) => {
     const info = await FileSystem.getInfoAsync(options.path);
     if (info.exists === false)
-      await FileSystem.makeDirectoryAsync(options.path, {intermediates: true});
+      await FileSystem.makeDirectoryAsync(options.path, {
+        intermediates: true,
+      });
     return FileSystem.writeAsStringAsync(pathForKey(key), JSON.stringify(value))
-    .then(() => {
-      callback && callback();
-    })
-    .catch(error => {
-      callback && callback(error);
-    });
+      .then(() => {
+        callback && callback();
+      })
+      .catch((error) => {
+        callback && callback(error);
+      });
   },
 
   getItem: async (key, callback) => {
     const info = await FileSystem.getInfoAsync(options.path);
     if (info.exists === false)
-      await FileSystem.makeDirectoryAsync(options.path, {intermediates: true});
+      await FileSystem.makeDirectoryAsync(options.path, {
+        intermediates: true,
+      });
     return FileSystem.readAsStringAsync(pathForKey(key))
-    .then(data => {
-      callback && callback(null, data);
-      if (!callback)
-        return JSON.parse(data);
-    })
-    .catch(error => {
-      callback && callback(error);
-      if (!callback)
-        throw error;
-    });
+      .then((data) => {
+        callback && callback(null, data);
+        if (!callback) return JSON.parse(data);
+      })
+      .catch((error) => {
+        callback && callback(error);
+        if (!callback) throw error;
+      });
   },
 
   removeItem: async (key, callback) => {
-    return FileSystem.deleteAsync(pathForKey(key), {idempotent: true})
-    .then(() => {
-      callback && callback();
-    });
+    return FileSystem.deleteAsync(pathForKey(key), { idempotent: true }).then(
+      () => {
+        callback && callback();
+      }
+    );
   },
 };
 
